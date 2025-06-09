f68e26a11546b5e1567a5f1c6b07e17b9ee982ac
Kingsley Chukure <151749239+Kingsley-net@users.noreply.github.com>
2025-06-09 22:37:08 +0100

    Update welcome.jsx

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -6,11 +6,11 @@ export function Welcome(){
     const navigate = useNavigate()
 return(
     <>
-    <div className='h-screen overflow-hidden bg-gray-200'>
+    <div className='h-screen w-full overflow-hidden bg-gray-200'>
 
 <div className='flex flex-col justify-center items-center h-full w-full'>
     <h1 className='font-bold text-4xl'>WeatherPulse</h1>
-    <p className='text-xl'>Discover accurate weather forecast with style</p>
+    <p className='text-xl text-center'>Discover accurate weather forecast</p>
     <img src={wel} className="h-auto  w-1/2"/>
     <button onClick={()=> navigate('/Home')} className='p-2 bg-blue-400 w-4/5 mt-9 text-white font-bold rounded-md'>Explore</button>
 </div>
