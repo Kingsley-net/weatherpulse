ce581f73d62dc418d4bfd5e8bb61482ac4348f76
Kingsley Chukure <151749239+Kingsley-net@users.noreply.github.com>
2025-06-09 22:14:10 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import autoprefixer from 'autoprefixer'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    autoprefixer()
-
   ],
   css: {
-    postcss: './postcss.config.cjs'
-  }
-})
+    postcss: './postcss.config.cjs',
+  },
+});
