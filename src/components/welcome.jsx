36538882f650cd86f58214f8107813d0dd6c3894
Kingsley Chukure <kchukure@gmail.com>
2025-07-05 16:46:18 +0000

    removed pics

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -11,8 +11,7 @@ return(
 <div className='flex flex-col justify-center items-center h-full w-full'>
     <h1 className='font-bold text-4xl'>WeatherPulse</h1>
     <p className='text-xl text-center'>Discover accurate weather forecast</p>
-    <img src={wel} className="h-auto  w-1/2"/>
-    <button onClick={()=> navigate('/Home')} className='p-2 bg-gradient-to-r bg-blue-400 w-4/5 mt-9 text-white font-bold rounded-md'>Explore</button>
+  <button onClick={()=> navigate('/Home')} className='p-2 bg-gradient-to-r bg-blue-400 w-4/5 mt-9 text-white font-bold rounded-md'>Explore</button>
 </div>
         </div>
         </>
